0f985978cb6978f0109d72ae62673c0c29dd22b9
Milana <94888u@gmail.com>
2024-04-25 11:57:02 +0200

    add simple user registration

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -1,17 +1,28 @@
 import asyncio
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
 from core.db.db_helper import db_helper
+from core.db.models import *
 from config import settings
 
 bot = Bot(settings.http_api_token)
 dp = Dispatcher()
 
 
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    async with db_helper.session_factory() as session:
+        session: AsyncSession
+        user = await session.get(TelegramUser, message.from_user.id)
+        if user:
+            await message.answer("Registered")
+        else:
+            session.add(TelegramUser(id=message.from_user.id))
+            await session.commit()
 
 
 async def main() -> None:
