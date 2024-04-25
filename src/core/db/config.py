c25c91c9490cbd809c72e4ba8216cf6267d58469
Milana <94888u@gmail.com>
2024-04-25 22:35:14 +0200

    add different db engines

--- src/core/db/config.py
+++ src/core/db/config.py
@@ -1,10 +1,50 @@
-from pydantic import Field
+from typing import Literal
+from abc import abstractmethod
+from functools import cached_property
+
+from pydantic import SecretStr, FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class SqlEngineSettings(BaseSettings):
+    @abstractmethod
+    def get_url(self) -> str:
+        raise NotImplementedError
+
+
+class PostgresSettings(SqlEngineSettings):
+    db: str
+    user: str
+    password: SecretStr
+
+    model_config = SettingsConfigDict(env_prefix="POSTGRES_")
+
+    def get_url(self) -> str:
+        return f"postgresql+pg8000://{self.user}:{self.password}@localhost/{self.db}"
+
+
+class SqliteSettings(SqlEngineSettings):
+    file_path: FilePath
+
+    model_config = SettingsConfigDict(env_prefix="SQLITE_")
+
+    def get_url(self) -> str:
+        return f"sqlite+aiosqlite:///{self.file_path}"
+
+
 class Settings(BaseSettings):
-    url: str = Field()
-    echo: bool = Field()
+    engine: Literal["sqlite", "postgresql"]
+    echo: bool
+
+    @cached_property
+    def url(self) -> str:
+        engine_settings_factory: type[SqlEngineSettings] = SqlEngineSettings
+        match self.engine:
+            case "sqlite":
+                engine_settings_factory = SqliteSettings
+            case "postgresql":
+                engine_settings_factory = PostgresSettings
+        return engine_settings_factory().get_url()
 
     model_config = SettingsConfigDict(env_prefix="SQL_DB_")
 
