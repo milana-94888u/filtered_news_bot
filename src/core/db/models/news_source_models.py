c8c8c3b0749db7d054d06024edb1766f58f4cab5
Milana <94888u@gmail.com>
2024-05-12 22:13:57 +0200

    Fix news source models schemas

--- src/core/db/models/news_source_models.py
+++ src/core/db/models/news_source_models.py
@@ -3,27 +3,38 @@ from typing import Any
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, declared_attr
 
-from core.db.sql_types import int_primary_key, str_with_length
+from core.db.sql_types import str_with_length
 from .base import BaseModel
 
 
 class NewsSource(BaseModel):
-    id: Mapped[int_primary_key]
-    source_type: Mapped[str_with_length(50)]
+    source_type: Mapped[str_with_length(50)] = mapped_column(primary_key=True)
+    unique_name: Mapped[str_with_length(50)] = mapped_column(primary_key=True)
 
-    @classmethod
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source_type = self.__class__.__name__
+
     @declared_attr.directive
     def __mapper_args__(cls: type["NewsSource"]) -> dict[str, Any]:
-        if cls.__name__ == "Employee":
+        if cls.__name__ == "NewsSource":
             return {
-                "polymorphic_on": cls.source_type,
                 "polymorphic_identity": cls.__name__,
+                "polymorphic_on": cls.source_type,
             }
         else:
-            return {"polymorphic_identity": cls.__name__}
+            return {
+                "polymorphic_identity": cls.__name__,
+                "inherit_condition": cls.source_type == "TelegramChannelSource"
+                and cls.unique_name == NewsSource.unique_name,
+            }
 
 
-class TelegramChannelSource(NewsSource):
-    id: Mapped[int_primary_key] = mapped_column(ForeignKey(NewsSource.id))
-    channel_handle: Mapped[str_with_length(50)]
+class PublicTelegramChannelNewsSource(NewsSource):
+    source_type: Mapped[str_with_length(50)] = mapped_column(
+        ForeignKey(NewsSource.source_type), primary_key=True
+    )
+    unique_name: Mapped[str_with_length(50)] = mapped_column(
+        ForeignKey(NewsSource.unique_name), primary_key=True
+    )
     last_post_id: Mapped[int]
